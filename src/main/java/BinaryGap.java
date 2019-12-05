2774b6f9193c277c6cb3ccf75d053097994aedca
shienacadiz <shienacadiz@gmail.com>
2019-12-05 19:10:11 +0800

    BinaryGap

---
+++ src/main/java/BinaryGap.java
@@ -0,0 +1,40 @@
+
+public class BinaryGap {
+
+	public static String convertToBinary(int num) {		
+		if(num/2 < 1) {
+			return Integer.toString(num % 2); 
+		}
+		else {
+			return convertToBinary(num/2) + (num%2);
+		}
+	}
+
+	public static int getBinaryGap(String string) {
+		int largestCount = 0;
+		boolean hasFoundOne = false;
+		int zeroCount = 0;
+
+		for(int x=0 ; x<string.length() ; x++) {
+			if("1".equals(String.valueOf(string.charAt(x)))) {
+				if(zeroCount > 0 && hasFoundOne) {
+					if(zeroCount>largestCount) {
+						largestCount = zeroCount;
+					}
+					zeroCount = 0;
+				}
+				hasFoundOne = true;
+			}
+			else if(hasFoundOne) {
+				zeroCount++;
+			}
+		}
+		
+		return largestCount;
+	}
+	
+	
+    public int solution(int N) {
+        return getBinaryGap(convertToBinary(N));        
+    }
+}
---
+++ src/test/java/BinaryGapTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class BinaryGapTest {
+
+	@Test
+	public void testConvertToBinary() {
+		assertEquals("1011", BinaryGap.convertToBinary(11));
+		assertEquals("10", BinaryGap.convertToBinary(2));
+		assertEquals("1111101000", BinaryGap.convertToBinary(1000));
+		assertEquals("110111100", BinaryGap.convertToBinary(444));
+		assertEquals("10000010001", BinaryGap.convertToBinary(1041));
+	}
+	
+	@Test
+	public void testGetBinaryGap() {
+		assertEquals(1, BinaryGap.getBinaryGap("1011"));
+		assertEquals(0, BinaryGap.getBinaryGap("10"));
+		assertEquals(1, BinaryGap.getBinaryGap("1111101000"));
+		assertEquals(1, BinaryGap.getBinaryGap("110111100"));
+		assertEquals(5, BinaryGap.getBinaryGap("10000010001"));
+	}
+}
