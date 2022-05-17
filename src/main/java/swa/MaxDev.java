81ec9e8b3f883dc7169603f293d2c0f107ab1c01
cadizsh <cadizsh@MFCGD.COM>
2022-05-17 12:54:29 +0800

    20220516 fixes on MaxDev

--- src/main/java/swa/MaxDev.java
+++ src/main/java/swa/MaxDev.java
@@ -1,30 +1,63 @@
 package swa;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MaxDev {
+
+    public static int viaLastIndex(String s) {
+        char[] stringAsChars = s.toCharArray();
+        Arrays.sort(stringAsChars);
+        String sortedString = String.valueOf(stringAsChars);
+
+        List<Integer> countList = new ArrayList<>();
+        for(int x=0; x<sortedString.length() ; ) {
+            int firstIndexOfChar = sortedString.indexOf(stringAsChars[x], x);
+            int lastIndexOfChar = sortedString.lastIndexOf(stringAsChars[x], firstIndexOfChar);
+            if (firstIndexOfChar == lastIndexOfChar) {
+                countList.add(1);
+            } else {
+                countList.add(lastIndexOfChar-firstIndexOfChar);
+            }
+            x = x + lastIndexOfChar + 1;
+        }
+        Collections.sort(countList);
+        return deductMinFromMax(countList);
+    }
+
     public static int getMaxFreqDeviation(String s) {
         char[] stringAsChars = s.toCharArray();
         Arrays.sort(stringAsChars);
-        int min = 0;
-        int max = 0;
 
+        List<Integer> countList = new ArrayList<>();
         for(int x=0; x<stringAsChars.length ; ) {
             int counter = 0;
-            for(int y=x; y<stringAsChars.length-1 ; y++) {
+            for(int y=x; y<stringAsChars.length ; y++) {
                 if(stringAsChars[x] == stringAsChars[y]) {
                     counter++;
+                    if(x+counter >= stringAsChars.length){
+                        x+=counter;
+                    }
                     continue;
-                } else if(counter > max) {
-                    max = counter;
-                } else if(counter<max && counter<min) {
-                    min = counter;
                 }
                 x+=counter;
                 break;
             }
+            countList.add(counter);
         }
+        Collections.sort(countList);
+        return deductMinFromMax(countList);
+    }
 
-        return max-min;
+    private static int deductMinFromMax(List<Integer> countList) {
+        if (countList.isEmpty()) {
+            return 0;
+        } else if (countList.get(0).equals(countList.get(countList.size() - 1))) {
+            return countList.get(0);
+        } else {
+            return countList.get(countList.size()-1) - countList.get(0);
+        }
     }
 }
