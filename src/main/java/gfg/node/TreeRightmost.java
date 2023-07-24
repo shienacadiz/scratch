4789b28bb2d2930cbaba9ec9dde0c8b7587a061a
shienacadiz <shienacadiz@gmail.com>
2023-07-24 22:45:25 +0800

    20230724 revised

--- src/main/java/gfg/node/TreeRightmost.java
+++ src/main/java/gfg/node/TreeRightmost.java
@@ -1,7 +1,6 @@
 package gfg.node;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.TreeMap;
@@ -10,17 +9,14 @@ public class TreeRightmost {
     private ArrayList<Integer> rightmostList = new ArrayList<>();
 
     private Map<Integer, Integer> levelMap = new TreeMap<>();
-    ArrayList<Integer> rightView(TreeNode node) {
+    public ArrayList<Integer> rightView(TreeNode node) {
         getRightMost(node, 1);
-        for(Map.Entry<Integer, Integer> cur : levelMap.entrySet()) {
-            rightmostList.add(cur.getValue());
-        }
-        return rightmostList;
+
+        return new ArrayList<>(levelMap.values());
     }
 
     private void getRightMost(TreeNode currentNode, int depth) {
         if(Objects.nonNull(currentNode)) {
-            System.out.println("Value: " + currentNode + "Depth: " + depth);
             levelMap.putIfAbsent(depth, currentNode.data);
             if (Objects.nonNull(currentNode.right)) {
                 getRightMost(currentNode.right, depth+1);
